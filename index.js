c3a57616bb481790aad94c5922e85e2818dfbc67
basel <baselhadrous99@gmail.com>
2020-12-08 17:27:31 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,93 @@
+const JSON = [
+	{
+		"id": "0001",
+		"type": "donut",
+		"name": "Cake",
+		"ppu":0.78,
+		"batters":
+			{
+				"batter":
+					[
+						{ "id": "1001", "type": "Regular" },
+						{ "id": "1002", "type": "Chocolate" },
+						{ "id": "1003", "type": "Blueberry" },
+						{ "id": "1004", "type": "Devil's Food" }
+					]
+			},
+		"topping":
+			[
+				{ "id": "5001", "type": "None" },
+				{ "id": "5002", "type": "Glazed" },
+				{ "id": "5005", "type": "Sugar" },
+				{ "id": "5007", "type": "Powdered Sugar" },
+				{ "id": "5006", "type": "Chocolate with Sprinkles" },
+				{ "id": "5003", "type": "Chocolate" },
+				{ "id": "5004", "type": "Maple" }
+			]
+	},
+	{
+		"id": "0002",
+		"type": "donut",
+		"name": "Raised",
+		"ppu": 0.55,
+		"batters":
+			{
+				"batter":
+					[
+						{ "id": "1001", "type": "Regular" }
+					]
+			},
+		"topping":
+			[
+				{ "id": "5001", "type": "None" },
+				{ "id": "5002", "type": "Glazed" },
+				{ "id": "5005", "type": "Sugar" },
+				{ "id": "5003", "type": "Chocolate" },
+				{ "id": "5004", "type": "Maple" }
+			]
+	},
+	{
+		"id": "0003",
+		"type": "donut",
+		"name": "Old Fashioned",
+		"ppu":0.26,
+		"batters":
+			{
+				"batter":
+					[
+						{ "id": "1001", "type": "Regular" },
+						{ "id": "1002", "type": "Chocolate" }
+					]
+			},
+		"topping":
+			[
+				{ "id": "5001", "type": "None" },
+				{ "id": "5002", "type": "Glazed" },
+				{ "id": "5003", "type": "Chocolate" },
+				{ "id": "5004", "type": "Maple" }
+			]
+	}
+];
+
+var counter = 0;
+var PPuTotal = 0;
+var PPuAvg = 0;
+
+JSON.forEach(function(obj) { 
+    counter += 1;
+    PPuTotal += obj.ppu;
+    obj.topping.forEach(function(innerObj) { 
+        console.log('topping ' + innerObj.type);
+    })
+    obj.batters.batter.forEach(function(innerObj) {
+        console.log('batters ' + innerObj.type);
+    })
+});
+
+PPuAvg = PPuTotal / counter;
+console.log('PPuAvg: ' + PPuAvg); 
+console.log('PPuTotal: ' + PPuTotal);
+
+JSON.forEach(function(obj) { 
+    console.log(obj.id); 
+});
